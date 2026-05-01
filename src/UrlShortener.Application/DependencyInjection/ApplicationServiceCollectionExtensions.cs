7a9ca0401b8dcaf4b919d0dd2a56fb61b2fb86f5
Javier Vallejos <51408883+jnvallejos@users.noreply.github.com>
2026-05-01 20:15:25 -0300

    feat(application): register admin use cases in addapplication

--- src/UrlShortener.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
+++ src/UrlShortener.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Application.ShortUrls.Admin.Disable;
+using UrlShortener.Application.ShortUrls.Admin.Enable;
+using UrlShortener.Application.ShortUrls.Admin.UpdateExpiration;
 using UrlShortener.Application.ShortUrls.Create;
 using UrlShortener.Application.ShortUrls.GetByCode;
 using UrlShortener.Application.ShortUrls.Redirect;
@@ -12,6 +15,9 @@ public static class ApplicationServiceCollectionExtensions
         services.AddScoped<CreateShortUrlUseCase>();
         services.AddScoped<RedirectUseCase>();
         services.AddScoped<GetShortUrlUseCase>();
+        services.AddScoped<DisableShortUrlUseCase>();
+        services.AddScoped<EnableShortUrlUseCase>();
+        services.AddScoped<UpdateExpirationUseCase>();
 
         return services;
     }
